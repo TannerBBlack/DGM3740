0c765308bfba30820aee76953b8d582f8376514f
Tanner Black <tbb777@gmail.com>
2021-04-02 13:52:23 -0600

    fixed the mobile menu

--- js/script.js
+++ js/script.js
@@ -24,36 +24,36 @@ x.onclick = toggleMenu;
 //     }
 // }
 
-function extractPageName(hrefString)
-{
-	var arr = hrefString.split('/');
-	return  (arr.length < 2) ? hrefString : arr[arr.length-2].toLowerCase() + arr[arr.length-1].toLowerCase();
-}
+// function extractPageName(hrefString)
+// {
+// 	var arr = hrefString.split('/');
+// 	return  (arr.length < 2) ? hrefString : arr[arr.length-2].toLowerCase() + arr[arr.length-1].toLowerCase();
+// }
 
-function setActiveMenu(arr, crtPage)
-{
-	for (var i=0; i < arr.length; i++)
-	{
-		if(extractPageName(arr[i].href) == crtPage)
-		{
-			if (arr[i].parentNode.tagName != "DIV")
-			{
-				arr[i].className = "active";
-				arr[i].parentNode.className = "active";
-			}
-		}
-	}
-}
+// function setActiveMenu(arr, crtPage)
+// {
+// 	for (var i=0; i < arr.length; i++)
+// 	{
+// 		if(extractPageName(arr[i].href) == crtPage)
+// 		{
+// 			if (arr[i].parentNode.tagName != "DIV")
+// 			{
+// 				arr[i].className = "active";
+// 				arr[i].parentNode.className = "active";
+// 			}
+// 		}
+// 	}
+// }
 
-function setPage()
-{
-	hrefString = document.location.href ? document.location.href : document.location;
+// function setPage()
+// {
+// 	hrefString = document.location.href ? document.location.href : document.location;
 
-	if (document.getElementById("primaryNav") !=null )
-	setActiveMenu(document.getElementById("primaryNav").getElementsByTagName("a"), extractPageName(hrefString));
-}
+// 	if (document.getElementById("primaryNav") !=null )
+// 	setActiveMenu(document.getElementById("primaryNav").getElementsByTagName("a"), extractPageName(hrefString));
+// }
 
-window.onload=function()
-{
-	setPage();
-}
+// window.onload=function()
+// {
+// 	setPage();
+// }
